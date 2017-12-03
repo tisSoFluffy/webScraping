434a44b81e2f28b572ce50c20f3f2633b6f5b27f
Josh Fritz <joshua.l.fritz@gmail.com>
2017-12-03 17:56:44 -0600

    init

---
+++ webScraping.py
@@ -0,0 +1,24 @@
+from urllib.request import urlopen
+from urllib.error import HTTPError
+from urllib.error import URLError
+
+from bs4 import BeautifulSoup
+
+
+def getTitle(url):
+    try:
+        html = urlopen(url)
+    except HTTPError as e:
+        return None
+    try:
+        bsObj = BeautifulSoup(html.read())
+        title = bsObj.body.h1
+    except AttributeError as e:
+        return None
+    return title
+title = getTitle("http://www.pythonscraping.com/pages/page1.html")
+
+if title == None:
+    print ("Title could not be found")
+else:
+    print(title)
