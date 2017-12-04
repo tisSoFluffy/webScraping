5ebd70e46309a82c26b13737be96506913ba1e18
Josh Fritz <joshua.l.fritz@gmail.com>
2017-12-03 18:53:51 -0600

    pages 2,3,4 added for Chapter 2.

---
+++ webScraping3.py
@@ -0,0 +1,11 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+html = urlopen("http://www.pythonscraping.com/pages/page3.html")
+bsObj = BeautifulSoup(html)
+
+for child in bsObj.find("table", {"id":"giftList"}).children:
+    print(child)
+
+for sibling in bsObj.find("table", {"id":"giftList"}).tr.next_siblings:
+    print(sibling)
